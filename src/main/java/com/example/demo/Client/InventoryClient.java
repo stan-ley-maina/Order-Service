85ed987645df2b9b199897ff1b1a440cc26f40b4
stanley <mainstano@gmail.com>
2025-01-25 23:52:15 +0300

    Second

--- src/main/java/com/example/demo/Client/InventoryClient.java
+++ src/main/java/com/example/demo/Client/InventoryClient.java
@@ -1,13 +1,27 @@
 package com.example.demo.Client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+import java.util.logging.Logger;
+
 @Component
+@Slf4j
 public interface InventoryClient {
 
 
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name="inventory", fallbackMethod="fallbackMethod")
+    @Retry(name="inventory")
     boolean isInStock(@RequestParam String skuCode,@RequestParam Integer quantity);
+
+    default  boolean fallbackMethod(String code,Integer quantity, Throwable throwable){
+
+    }
+
+
+
+
 }
